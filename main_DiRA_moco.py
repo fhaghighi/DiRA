11bc10efe4f7f851c0c6e95a54aedb6ce94d2106
fhaghighi <31901902+fhaghighi@users.noreply.github.com>
2023-02-09 16:28:50 -0700

    Update main_DiRA_moco.py

--- main_DiRA_moco.py
+++ main_DiRA_moco.py
@@ -39,7 +39,7 @@ parser.add_argument('-a', '--arch', metavar='ARCH', default='resnet50',
                         ' (default: resnet50)')
 parser.add_argument('-j', '--workers', default=16, type=int, metavar='N',
                     help='number of data loading workers (default: 16)')
-parser.add_argument('--epochs', default=1000, type=int, metavar='N',
+parser.add_argument('--epochs', default=1200, type=int, metavar='N',
                     help='number of total epochs to run')
 parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
                     help='manual epoch number (useful on restarts)')
